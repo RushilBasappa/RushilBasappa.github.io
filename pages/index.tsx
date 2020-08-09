563440bbb517bbf55f5dbe343e90388f892d92cd
Rushil Basappa <rushil.oucse37@gmail.com>
2020-08-09 12:06:46 +0530

    add export command

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const IndexPage: React.FC<{}> = (): JSX.Element => {
   return (
     <div>
-      <h1>Rushil Basappa!!</h1>
+      <h1>Rushil Basappa!</h1>
     </div>
   );
 };
