c4601a9ad508ca32668df810ee798cdea207a6f5
Rushil Basappa <rushil.oucse37@gmail.com>
2020-08-09 05:35:01 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage: React.FC<{}> = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Rushil Basappa!!</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
