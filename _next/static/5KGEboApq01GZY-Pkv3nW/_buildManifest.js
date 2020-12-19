04dd6b4df704f00b30e3e46b000330a24c1f66fe
RushilBasappa <RushilBasappa@users.noreply.github.com>
2020-12-19 20:24:41 +0000

    deploy: ba8887736b6cc7908e34b33cc7dcbc4a003cb688

---
+++ _next/static/5KGEboApq01GZY-Pkv3nW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1359f928ed9b069b7488.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f1850d205b8186c5ae24.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
